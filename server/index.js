997e5814d6508332ef10c4cfa6d42c9932aca379
Sabuj-Chowdhury <csabuj73@gmail.com>
2024-12-28 01:31:26 +0530

    route for delete a donation data from DB and route for donations data for that logged in user

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iehww.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.i53p4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -28,6 +28,24 @@ async function run() {
     app.post("/add-donation", async (req, res) => {
       const data = req.body;
       const result = await donationCollections.insertOne(data);
+      res.send(result);
+    });
+
+    // ***********************GET************************
+    app.get("/my-donations/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { "donner.email": email };
+      const result = await donationCollections.find(query).toArray();
+      res.send(result);
+    });
+
+    // ********************DELETE***************************
+
+    // route for delete a donation data from DB
+    app.delete("/donation/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await donationCollections.deleteOne(query);
       res.send(result);
     });
 
